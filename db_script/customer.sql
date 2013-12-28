58e903fce4c00cdf0143b8be110a5ee983f4b72b
SHARON <shynx_swear@yahoo.com>
2013-12-27 22:59:57 -0800

    [new]customer

---
+++ db_script/customer.sql
@@ -0,0 +1,9 @@
+﻿create table customer (
+ID char (9) not null,
+fname text not null,
+mname text not null,
+lname text not null,
+primary key (ID),
+address text not null,
+contact_N text not null,
+email char not null);
