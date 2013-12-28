eb7e9f91137a51e41a932e024f4609b8a6490a9d
SHARON <shynx_swear@yahoo.com>
2013-12-27 23:42:15 -0800

    [Return]

---
+++ db_script/return.sql
@@ -0,0 +1,7 @@
+﻿create table Return(
+date_returned date not null,
+movieid int not null,
+ID char (9) not null,
+rent_code int not null,
+foreign key (movieid) references moviedetail(movieid),
+foreign key (ID) references customer (ID));
