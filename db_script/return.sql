7a0b5f195c07884b3f7e599da287eef349a73005
SHARON <shynx_swear@yahoo.com>
2013-12-28 11:36:32 -0800

    [RETURN] new (12-28-13

--- db_script/return.sql
+++ db_script/return.sql
@@ -3,5 +3,6 @@ date_returned date not null,
 movieid int not null,
 ID char (9) not null,
 rent_code int not null,
+primary key (rent_code),
 foreign key (movieid) references moviedetail(movieid),
 foreign key (ID) references customer (ID));
