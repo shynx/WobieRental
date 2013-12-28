b3a492461d420c56c72c8d33db1fe971d6c90064
SHARON <shynx_swear@yahoo.com>
2013-12-27 23:34:37 -0800

    [Movie Rental]

---
+++ db_script/movierental.sql
@@ -0,0 +1,7 @@
+﻿create table MovieRental(
+movieid int not null,
+ID char(9) not null,
+rent_date date not null,
+price money not null,
+foreign key (movieid) references moviedetail(movieid),
+foreign key (ID) references customer (ID));
