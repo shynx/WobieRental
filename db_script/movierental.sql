949836ff39186d045bf9be91fef087520f3b6588
SHARON <shynx_swear@yahoo.com>
2013-12-28 11:27:00 -0800

    [MOVIERENTAL] new (12-28-13)

--- db_script/movierental.sql
+++ db_script/movierental.sql
@@ -3,5 +3,6 @@ movieid int not null,
 ID char(9) not null,
 rent_date date not null,
 price money not null,
+year_release int not null,
 foreign key (movieid) references moviedetail(movieid),
 foreign key (ID) references customer (ID));
