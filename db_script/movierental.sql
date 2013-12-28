029ca86476677775c86553269c616ae4f8c4e905
SHARON <shynx_swear@yahoo.com>
2013-12-28 13:41:10 -0800

    [MOVIERENTAL] update (12-28-13)

--- db_script/movierental.sql
+++ db_script/movierental.sql
@@ -1,8 +1,10 @@
 ﻿create table MovieRental(
+rental_code int not null,
 movieid int not null,
 ID char(9) not null,
 rent_date date not null,
 price money not null,
 year_release int not null,
+primary key (rental_code),
 foreign key (movieid) references moviedetail(movieid),
 foreign key (ID) references customer (ID));
